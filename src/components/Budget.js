2ec6cc0c159d41f62c93c862cda01c6ba62368d5
Alisa <alisa.luoma@hotmail.fi>
2023-03-17 11:08:50 +0200

    Upper limit has set

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,26 @@
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, remaining } = useContext(AppContext);
+    const [inputValue, setInputValue] = useState(budget);
+
+    const handleInputChange = (event) => {
+        const value = parseInt(event.target.value);
+        if(value > 20000) value = 20000;
+        setInputValue(value < remaining ? remaining : value);
+    };
+
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+        <div className="alert alert-secondary">
+            <span>Budget: </span>
+            <input
+                type="number"
+                className="form-control"
+                value={inputValue}
+                onChange={handleInputChange}
+                step="10.0"
+            ></input>
         </div>
     );
 };
